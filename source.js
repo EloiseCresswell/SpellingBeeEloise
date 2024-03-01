bd883d585bfbb764591a2c26c1d0702d26dfe654
Leigh and El <leighandel@Leighs-MacBook-Pro.local>
2024-03-01 14:27:54 +0000

    added buttons and their value

--- source.js
+++ source.js
@@ -1,11 +1,8 @@
 console.log("hello there!");
 let score = 0;
-
 //finding the answer when you submit the button
-
 //function to give 7 random letters...
 let characters = "BCDFGHJKLMNPQRSTVWXYZ";
-
 function fiveRandomConsts() {
   let randomLetters = "";
   for (let i = 0; i < 5; i++) {
@@ -17,7 +14,6 @@ function fiveRandomConsts() {
   }
   return randomLetters;
 }
-
 //Vowel function
 let vowels = "AEIOU";
 
@@ -32,14 +28,64 @@ function twoRandomVowels() {
   }
   return randomLettersVowels;
 }
-
 //combining the two functions together to produce the string for the letters
 function comboLetters() {
   const constnants = fiveRandomConsts();
   const vowels = twoRandomVowels();
-  document.getElementById("letters").innerHTML = constnants + vowels;
+  //document.getElementById("letters").innerHTML = constnants + vowels;
+  //printing each letter onto the corresponding button
+  let letterString = constnants + vowels;
+  /*let letterArray = letterString.split("").join(",");
+  //console.log(letterArray);
+  //document.getElementById("firstLet").innerHTML = letterString[0];
+  letterArray.forEach((element) => {
+    document.getElementById("firstLet").innerHTML = letterString[element];
+  });*/
+  let buttonone = document.getElementById("firstLet");
+  buttonone.innerHTML = letterString[0];
+  let buttontwo = document.getElementById("secondLet");
+  buttontwo.innerHTML = letterString[1];
+  let buttonthree = document.getElementById("thirdLet");
+  buttonthree.innerHTML = letterString[2];
+  let buttonfour = document.getElementById("forthLet");
+  buttonfour.innerHTML = letterString[3];
+  let buttonfive = document.getElementById("fifthLet");
+  buttonfive.innerHTML = letterString[4];
+  let buttonsix = document.getElementById("sixLet");
+  buttonsix.innerHTML = letterString[5];
+  let buttonseven = document.getElementById("sevenLet");
+  buttonseven.innerHTML = letterString[6];
+  //check if button 1 is clicked, if so put value in answer field
+  buttonone.addEventListener("click", function () {
+    let letter = buttonone.innerHTML;
+    document.getElementById("answer").value += letter;
+  });
+  //if button 2 is clicked
+  buttontwo.addEventListener("click", function () {
+    let letter = buttontwo.innerHTML;
+    document.getElementById("answer").value += letter;
+  });
+  buttonthree.addEventListener("click", function () {
+    let letter = buttonthree.innerHTML;
+    document.getElementById("answer").value += letter;
+  });
+  buttonfour.addEventListener("click", function () {
+    let letter = buttonfour.innerHTML;
+    document.getElementById("answer").value += letter;
+  });
+  buttonfive.addEventListener("click", function () {
+    let letter = buttonfive.innerHTML;
+    document.getElementById("answer").value += letter;
+  });
+  buttonsix.addEventListener("click", function () {
+    let letter = buttonsix.innerHTML;
+    document.getElementById("answer").value += letter;
+  });
+  buttonseven.addEventListener("click", function () {
+    let letter = buttonseven.innerHTML;
+    document.getElementById("answer").value += letter;
+  });
 }
-
 comboLetters();
 
 //check if combo letters are in the answer provided...
@@ -47,11 +93,11 @@ comboLetters();
 //function to check if the answer is correct / in the dictionary and run it if clicked
 let answerSubmit = document.getElementById("submitButton");
 answerSubmit.addEventListener("click", getAnswer);
+
 function getAnswer() {
   answer = document.getElementById("answer").value;
   const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + answer;
   checkDictionary();
-
   //changing the score based on answer length...
   //if <4 - score 1
   //if >4 - score 2
@@ -59,7 +105,6 @@ function getAnswer() {
   if (answer.length > 4) {
     additionToScore = 2;
   }
-
   //function to check if the given word is in fact a word in the dictionary
   async function checkDictionary() {
     const response = await fetch(url);
